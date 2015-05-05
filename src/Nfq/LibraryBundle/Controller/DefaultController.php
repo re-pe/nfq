bdcf6b9b7d6b87200f6ce682ad3bf45489603dc9
Redas Peskaitis <redas.peskaitis@gmail.com>
2015-05-06 01:20:27 +0300

    Pavyzdys, kaip apeiti Twigo apribojimus

--- src/Nfq/LibraryBundle/Controller/DefaultController.php
+++ src/Nfq/LibraryBundle/Controller/DefaultController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('NfqLibraryBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('NfqLibraryBundle:Default:index.html.twig');
+    }
+
+    public function helloAction($name)
+    {
+        return $this->render('NfqLibraryBundle:Default:hello.html.twig', array('name' => $name));
     }
 }
--- web/index.php
+++ web/index.php
@@ -1,3 +1,5 @@
 <?php
 //  header('Location: '. '/app_dev.php/bookdata/9789955383024');
-  header('Location: '. '/app_dev.php/search/Lands');
+//  header('Location: '. '/app_dev.php/search/Lands');
+  header('Location: '. '/app_dev.php');?>
+
